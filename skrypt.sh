471a0dadcd3734d65c7eaf5bf03d08e1895f77ac
Sofiia <dsw45394@student.dsw.edu.pl>
2022-05-18 15:54:38 +0200

    Updated Cz2_branch

--- skrypt.sh
+++ skrypt.sh
@@ -5,31 +5,52 @@ command=$1
 
 # FUNCTIONS
 create_txt() {
-	numFiles=$1  # first arg to func
+	type=$1      # type of file: log or error
+	numFiles=$2  # 2d arg to func
 	for i in `seq 1 $numFiles`
 	do
-		name="log${i}.txt"
-		echo $name > $name
-		echo $script_name >> $name
-		echo `date` >> $name
+		name="$type${i}"
+		filen="$name/$name.txt"
+		if [ ! -e $name ]; then    # if the dir already exists, just overwrite the txt inside it.
+			mkdir $name
+		fi
+
+		echo $filen > $filen
+		echo $script_name >> $filen
+		echo `date` >> $filen
 	done
 }
 
 # COMMANDS
-if [ $command = "--help" ]; then
-	echo "Available commands:   --help, --date, --logs"
-elif [ $command = "--date" ]; then
+if [ $command = "--help" ] || [ $command = "-h" ]; then
+	echo "Available commands:   --help / -h, --date / -d, --logs / -l, --error / -e, --init"
+elif [ $command = "--date" ] || [ $command = "-d" ]; then
 	data=`date`
 	echo $data
-elif [ $command = "--logs" ]; then
+elif [ $command = "--logs" ] || [ $command = "-l" ]; then
 	if [ "$2" ]; then
-		create_txt $2
-	else 
-		create_txt 100
+		create_txt "log" $2
+	else
+		create_txt "log" 100
 	fi
+elif [ $command = "--error" ] || [ $command = "-e" ]; then
+	if [ "$2" ]; then
+		create_txt "error" $2
+	else
+		create_txt "error" 100
+	fi
+elif [ $command = "--init" ]; then
+	if [ "$2" ]; then
+		git clone $2   # if the name of repo is given at start
+		export PATH=$PATH:$PWD
+	else 
+		echo "No repo name is given" 
+	fi
+
 else
 	echo "Wrong command"
 fi
 
 # UPDATING .GITIGNORE FILE
 ls | grep "log" > .gitignore
+ls | grep "error" >> .gitignore
